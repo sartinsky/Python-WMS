de9c6db5b7906ecc585f9293b7f048f709c504a0
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-06-13 11:08:15 +0300

    test15

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -74,6 +74,39 @@ def units_input(hashMap,_files=None,_data=None):
         
     return hashMap  
 
+def Get_OrderGoods_Data_To_Table(hashMap, _files=None, _data=None):
+    
+    # Путь к нужной таблице или представлению
+    path = 'wms_orders_table?'
+    
+    # Полный URL для запроса
+    url = f'{postgrest_url}/{path}'
+
+    # Параметры запроса (например, фильтрация данных)
+    params = {
+        'select': 'Товар:nom,Артикул:code,План:plan,Факт:fact&order_id=eq.~id~'
+    }
+
+    try:
+        # Логирование перед отправкой запроса
+        
+        # Отправка GET-запроса
+        response = requests.get(url, params=params)
+
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            # Парсинг JSON ответа
+            data = response.json()
+            hashMap.put("orders_table", json.dumps(data))
+        else:
+            hashMap.put("toast", f'Error: {response.status_code}')
+        
+    except Exception as e:
+        hashMap.put("toast", f'Exception occurred: {str(e)}')
+        #print(f'Исключение: {str(e)}')
+
+    return hashMap
+
 #Пример использования функции
 # class MockHashMap:
 #     def __init__(self):
