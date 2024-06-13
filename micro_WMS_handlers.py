7ccf20f74e682197f75b89256c5b2ad26eec687c
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-06-13 17:56:38 +0300

    test25

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -39,41 +39,69 @@ def Get_Orders_Data_To_Table(hashMap, _files=None, _data=None):
 
 def units_input(hashMap,_files=None,_data=None):
     
-    #jrecord = json.loads(hashMap.get("selected_line"))
-    #unit_id = str(jrecord['id'])
-    unit_id = '85'
+    if hashMap.get("listener")=="barcode":
+        barcode = hashMap.get("barcode")
+        path = 'wms_goods?barcode=in.(%22{barcode}~%22)'.format(barcode=barcode)
+         # Полный URL для запроса
+        url = f'{postgrest_url}/{path}'
 
-    # Путь к нужной таблице или представлению
-    path = 'wms_orders_captions?id=eq.{unit_id}'.format(unit_id=unit_id)
-    
-    # Полный URL для запроса
-    url = f'{postgrest_url}/{path}'
-    
-    try:
-        # Отправка GET-запроса
-        response = requests.get(url)
+        try:
+            # Отправка GET-запроса
+            response = requests.get(url)
 
-        # Проверка статуса ответа
-        if response.status_code == 200:
-            # Парсинг JSON ответа
-            data = response.json()
-            jrecord = data[0]
-            hashMap.put("order", jrecord['caption'])
-            hashMap.put("orderRef", unit_id)
-            Get_OrderGoods_Data_To_Table(hashMap)
-            hashMap.put("ShowScreen", "Приемка по заказу начало")
-        else:
-           hashMap.put("toast", f'Error: {response.status_code}')
-           #print(f'Ошибка запроса: {response.status_code} - {response.text}')
-    except Exception as e:
-        hashMap.put("toast", f'Exception occurred: {str(e)}')
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                # Парсинг JSON ответа
+                data = response.json()
+                if data:
+                    jrecord = data[0]
+                    hashMap.put("nom", jrecord['caption'])
+                    hashMap.put("art", jrecord['code'])
+                    hashMap.put("nom_id", jrecord['id'])
+                    hashMap.put("unit", jrecord['unit_str'])
+                    hashMap.put("ShowScreen", "wms.Ввод количества факт по заказу")                    
+                else:    
+                    hashMap.put("toast", "Товар не найден")        
+            else:
+                hashMap.put("toast", f'Error: {response.status_code}')
+            
+        except Exception as e:
+            hashMap.put("toast", f'Exception occurred: {str(e)}')
+
+    else:
+        jrecord = json.loads(hashMap.get("selected_line"))
+        unit_id = str(jrecord['id'])
+        
+        # Путь к нужной таблице или представлению
+        path = 'wms_orders_captions?id=eq.{unit_id}'.format(unit_id=unit_id)
+        
+        # Полный URL для запроса
+        url = f'{postgrest_url}/{path}'
+        
+        try:
+            # Отправка GET-запроса
+            response = requests.get(url)
+
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                # Парсинг JSON ответа
+                data = response.json()
+                jrecord = data[0]
+                hashMap.put("order", jrecord['caption'])
+                hashMap.put("orderRef", unit_id)
+                Get_OrderGoods_Data_To_Table(hashMap)
+                hashMap.put("ShowScreen", "Приемка по заказу начало")
+            else:
+            hashMap.put("toast", f'Error: {response.status_code}')
+            #print(f'Ошибка запроса: {response.status_code} - {response.text}')
+        except Exception as e:
+            hashMap.put("toast", f'Exception occurred: {str(e)}')
         
     return hashMap  
 
 def Get_OrderGoods_Data_To_Table(hashMap, _files=None, _data=None):
 
-    #unit_id = hashMap.get("orderRef") 
-    unit_id = 85
+    unit_id = hashMap.get("orderRef") 
     # Путь к нужной таблице или представлению
     path = 'wms_orders_table?select=Товар:nom,Артикул:code,План:plan,Факт:fact&order_id=eq.{unit_id}'.format(unit_id=unit_id)
     
@@ -105,6 +133,11 @@ def Get_OrderGoods_Data_To_Table(hashMap, _files=None, _data=None):
 def Set_Var(hashMap, _files=None, _data=None):
 
     hashMap.put("noaddr", 'true')
+    return hashMap
+
+def goods_record_input(hashMap,_files=None,_data=None):
+    global nom_id
+
     return hashMap
 
 # #Пример использования функции
