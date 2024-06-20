df7d7f5f4fc30c65dfd25c41904913cd3685a86d
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-06-20 19:33:38 +0300

    test58

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -145,8 +145,7 @@ def Set_Var(hashMap, _files=None, _data=None):
     elif CurScreen=="wms.Ввод количества факт по заказу":
         hashMap.put("noaddr", 'true')
     elif CurScreen=="wms.Ввод количества взять размещение":
-        hashMap.put("noaddr", 'true')
-        hashMap.put("toast", hashMap.get("current_screen_name"))
+        hashMap.put("noaddr", 'true')        
     return hashMap
 
 def goods_record_input(hashMap,_files=None,_data=None):
