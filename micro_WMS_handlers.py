4d2fda5cb35f5f23086c4cf2bdecdcbb7877997d
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-06-24 18:47:08 +0300

    test63

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -363,6 +363,7 @@ def get_placement_orders(hashMap, _files=None, _data=None):
 
 def on_address_input(hashMap,_files=None,_data=None):
     
+    hashMap.put("toast", "111")
     #CurScreen = hashMap.get("current_screen_name")
     listener = hashMap.get("listener")
 
