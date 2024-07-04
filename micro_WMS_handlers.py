3111f0a6aadba0403d7121968effd3655e0960a1
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-07-04 09:46:21 +0300

    test11

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -2,7 +2,7 @@ import requests
 import json
 
  # URL вашего PostgREST сервера
-postgrest_url = 'http://192.168.1.26:3000'
+postgrest_url = 'http://192.168.1.27:3000'
 timeout = 3
 
 def init_on_start(hashMap,_files=None,_data=None):
