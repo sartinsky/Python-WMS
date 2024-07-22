247a7311df6866beaf96dd777f7b6c88a19d4349
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-07-22 13:01:05 +0300

    test200

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -118,13 +118,22 @@ def Get_Orders_Data_To_Table(hashMap, _files=None, _data=None):
 
     # Путь к нужной таблице или представлению
     if CurScreen == 'wms.Выбор распоряжения':        
-        path = 'wms_orders_captions?and=(typeid.eq.1,or(done.neq.true,done.is.null))&select=id:id,Поставщик:contractor,Номер:doc_number'
+        if user_locale == 'ua':
+            path = 'wms_orders_captions?and=(typeid.eq.1,or(done.neq.true,done.is.null))&select=id:id,Постачальник:contractor,Номер:doc_number'
+        elif user_locale == 'ru':
+            path = 'wms_orders_captions?and=(typeid.eq.1,or(done.neq.true,done.is.null))&select=id:id,Поставщик:contractor,Номер:doc_number'
         
     elif CurScreen == 'wms.Выбор распоряжения отбор':
-        path = 'wms_orders_captions?and=(typeid.eq.2,done.is.null)&select=id:id,Покупатель:contractor,Номер:doc_number'
+        if user_locale == 'ua':
+            path = 'wms_orders_captions?and=(typeid.eq.2,done.is.null)&select=id:id,Покупець:contractor,Номер:doc_number'
+        elif user_locale == 'ru':
+            path = 'wms_orders_captions?and=(typeid.eq.2,done.is.null)&select=id:id,Покупатель:contractor,Номер:doc_number'
 
     elif CurScreen == 'wms.Выбор распоряжения отгрузка':
-        path = 'wms_outgoing?select=id:id,Покупатель:contractor,Номер:doc_number'    
+        if user_locale == 'ua':
+            path = 'wms_outgoing?select=id:id,Покупець:contractor,Номер:doc_number'
+        elif user_locale == 'ru':
+            path = 'wms_outgoing?select=id:id,Покупатель:contractor,Номер:doc_number'
 
     elif CurScreen == 'wms.Выбор распоряжения инвентаризация':
         path = 'wms_orders_captions?and=(typeid.eq.3,done.is.null)&select=id:id,Склад:contractor,Дата:doc_date_str'
@@ -158,10 +167,16 @@ def Get_OrderGoods_Data_To_Table(hashMap, _files=None, _data=None):
         path = f'wms_orders_table?select=id:sku_id,Товар:nom,Артикул:code,План:plan,Факт:fact&order_id=eq.{order_id}'
                 
     elif CurScreen == 'wms.Ввод адреса отбор':
-        path = f'rpc/get_picking?orderid={order_id}&select=id:sku_id,Товар:sku,Адрес:address,Кол-во:qty'
+        if user_locale == 'ua':
+            path = f'rpc/get_picking?orderid={order_id}&select=id:sku_id,Товар:sku,Адреса:address,Кіл-ть:qty'
+        elif user_locale == 'ru':
+            path = f'rpc/get_picking?orderid={order_id}&select=id:sku_id,Товар:sku,Адрес:address,Кол-во:qty'
 
     elif CurScreen == 'wms.Ввод товара отгрузка':
-        path = f'wms_outgoing_table?select=id:sku_id,Товар:caption,Артикул:code,Осталось отгрузить:qty&order_id=eq.{order_id}&qty=neq.0'
+        if user_locale == 'ua':
+            path = f'wms_outgoing_table?select=id:sku_id,Товар:caption,Артикул:code,Залишилось відвантажити:qty&order_id=eq.{order_id}&qty=neq.0'
+        elif user_locale == 'ru':
+            path = f'wms_outgoing_table?select=id:sku_id,Товар:caption,Артикул:code,Осталось отгрузить:qty&order_id=eq.{order_id}&qty=neq.0'
 
     elif CurScreen == 'wms.Выбор распоряжения инвентаризация':
         path = f'rpc/get_inventory_list?orderid={order_id}&select=Товар:nom,План:qty_plan,Факт:qty_fact'
@@ -230,7 +245,7 @@ def Get_Picking(hashMap, _files=None, _data=None):
                     jrecord = data[0]
                     hashMap.put("NextAddr", jrecord['address'])
                 else:
-                    hashMap.put("toast", 'Нет данных по пиккингу')    
+                    hashMap.put("toast", 'Нет данных по пиккингу' if user_locale == 'ru' else 'Нема даних по пікінгу')    
             else:
                 hashMap.put("toast", f'Error: {response.status_code}')
                 
@@ -244,7 +259,10 @@ def get_operators_placing(hashMap, _files=None, _data=None):
     #user = hashMap.get("ANDROID_ID") 
     user = 'К РАЗМЕЩЕНИЮ' 
     # Путь к нужной таблице или представлению
-    path = f'rpc/get_operators_placing?user_id={user}&select=id:sku_id,Товар:nom,Кол-во:qty,order_id:id'
+    if user_locale == 'ua':
+        path = f'rpc/get_operators_placing?user_id={user}&select=id:sku_id,Товар:nom,Кіл-ть:qty,order_id:id'
+    elif user_locale == 'ru':
+        path = f'rpc/get_operators_placing?user_id={user}&select=id:sku_id,Товар:nom,Кол-во:qty,order_id:id'
     
     # Полный URL для запроса
     url = f'{postgrest_url}/{path}'
@@ -282,7 +300,10 @@ def get_placement_orders(hashMap, _files=None, _data=None):
 
     user = hashMap.get("ANDROID_ID") 
     # Путь к нужной таблице или представлению
-    path = f'rpc/get_placement_orders?user_id={user}&select=Товар:sku,Ячейка:address,Кол-во:qty,id:sku_id'
+    if user_locale == 'ua':
+        path = f'rpc/get_placement_orders?user_id={user}&select=Товар:sku,Комірка:address,Кіл-ть:qty,id:sku_id'
+    elif user_locale == 'ru':
+        path = f'rpc/get_placement_orders?user_id={user}&select=Товар:sku,Ячейка:address,Кол-во:qty,id:sku_id'
             
     # Полный URL для запроса
     url = f'{postgrest_url}/{path}'
@@ -319,7 +340,10 @@ def get_goods_for_address_placement(hashMap, _files=None, _data=None):
     user = hashMap.get("ANDROID_ID") 
     address = hashMap.get("addr_id") 
     # Путь к нужной таблице или представлению
-    path = f'rpc/get_goods_for_address_placement?user_id={user}&address_id={address}&select=Товар:sku,Кол-во:qty'
+    if user_locale == 'ua':
+        path = f'rpc/get_goods_for_address_placement?user_id={user}&address_id={address}&select=Товар:sku,Кіл-ть:qty'
+    elif user_locale == 'ru':
+        path = f'rpc/get_goods_for_address_placement?user_id={user}&address_id={address}&select=Товар:sku,Кол-во:qty'
            
     # Полный URL для запроса
     url = f'{postgrest_url}/{path}'
@@ -346,7 +370,10 @@ def get_operators_moving(hashMap, _files=None, _data=None):
 
     user = hashMap.get("ANDROID_ID") 
     # Путь к нужной таблице или представлению
-    path = f'rpc/get_operators_moving?user_id={user}&select=Товар:nom,Кол-во:qty'
+    if user_locale == 'ua':
+        path = f'rpc/get_operators_moving?user_id={user}&select=Товар:nom,Кіл-ть:qty'
+    elif user_locale == 'ru':
+        path = f'rpc/get_operators_moving?user_id={user}&select=Товар:nom,Кол-во:qty'
             
     # Полный URL для запроса
     url = f'{postgrest_url}/{path}'
@@ -403,20 +430,6 @@ def on_FORVARD_BUTTON(hashMap, _files=None, _data=None):
     CurScreen = hashMap.get("current_screen_name")
     if CurScreen == "Приемка по заказу начало" and listener is None:
         hashMap.put("ShowScreen", "wms.Ввод товара по заказу")
-    # elif CurScreen == "wms.Ввод количества размещение":
-    #     hashMap.put("ShowScreen", "wms.Ввод товара размещение")
-    # elif CurScreen == "wms.Ввод количества взять размещение":
-    #     hashMap.put("ShowScreen", "wms.Ввод товара размещение взять")
-    # elif CurScreen == "wms.Ввод товара перемещение":
-    #     hashMap.put("ShowScreen", "wms.Ввод адреса")    
-    # elif CurScreen == "wms.Ввод товара положить":
-    #     hashMap.put("ShowScreen", "wms.Ввод адреса")        
-    # elif CurScreen == "wms.Ввод количества положить":
-    #     hashMap.put("ShowScreen", "wms.Ввод товара положить")
-    # elif CurScreen == "wms.Ввод товара отбор":
-    #     hashMap.put("ShowScreen", "wms.Ввод адреса отбор")
-    # elif CurScreen=="wms.Ввод количества отбор":
-    #     hashMap.put("ShowScreen", "wms.Ввод товара отбор")                
     return hashMap 
 
 def on_btn_put(hashMap, _files=None, _data=None):
@@ -466,7 +479,10 @@ def on_btn_done(hashMap,_files=None,_data=None):
             if CurScreen == 'wms.Ввод адреса инвентаризация':
                 on_input_qtyfact(hashMap)            
             
-            hashMap.put("toast", 'Документ завершен')
+            if user_locale == 'ua':
+                hashMap.put("toast", 'Документ завершено')
+            elif user_locale == 'ru':
+                hashMap.put("toast", 'Документ завершен')
             hashMap.put("FinishProcess","")        
         else:
             hashMap.put("toast", f'Error: {response.status_code}')
@@ -513,7 +529,10 @@ def on_address_input(hashMap,_files=None,_data=None):
                     elif CurScreen == 'wms.Ввод адреса инвентаризация':
                         hashMap.put("ShowScreen", "wms.Ввод товара инвентаризация")    
                 else:    
-                    hashMap.put("toast", f"Ячейка с штрихкодом {barcode} не найдена")        
+                    if user_locale == 'ua':
+                        hashMap.put("toast", f"Комірку з штрихкодом {barcode} не знайдено")
+                    elif user_locale == 'ru':
+                        hashMap.put("toast", f"Ячейка с штрихкодом {barcode} не найдена")
             else:
                 hashMap.put("toast", f'Error: {response.status_code}')
             
@@ -587,7 +606,10 @@ def on_input_qtyfact(hashMap,_files=None,_data=None):
             filtered_data = json.loads(hashMap.get('filtered_data'))
             total_qty = int(hashMap.get("qty"))
             for row in filtered_data:
-                cur_qty = min(total_qty,row['Кол-во'])
+                if user_locale == 'ua':
+                    cur_qty = min(total_qty,row['Кіл-ть'])
+                elif user_locale == 'ru':
+                    cur_qty = min(total_qty,row['Кол-во'])
                 order_id = row['order_id']
 
                 hashMap.put("qty_minus", str(-1*cur_qty))
@@ -1112,7 +1134,10 @@ def on_units_input(hashMap,_files=None,_data=None):
                             hashMap.put("art", '')
                             hashMap.put("nom_id", '')
                             hashMap.put("unit", '')
-                            hashMap.put("toast", 'Указанный товар отсутствует списке на забор для размещения') 
+                            if user_locale == 'ua':
+                                hashMap.put("toast", 'Вказаний товар відсутній у списку на забір для розміщення') 
+                            elif user_locale == 'ru':
+                                hashMap.put("toast", 'Указанный товар отсутствует в списке на забор для размещения') 
                             hashMap.put("ShowScreen", "wms.Ввод товара размещение взять")
                         else:
                             hashMap.put("filtered_data", json.dumps(filtered_data))
@@ -1127,7 +1152,10 @@ def on_units_input(hashMap,_files=None,_data=None):
                             hashMap.put("art", '')
                             hashMap.put("nom_id", '')
                             hashMap.put("unit", '')
-                            hashMap.put("toast", 'Указанный товар отсутствует списке на размещение') 
+                            if user_locale == 'ua':
+                                hashMap.put("toast", 'Вказаний товар відсутній у списку на розміщення') 
+                            elif user_locale == 'ru':
+                                hashMap.put("toast", 'Указанный товар отсутствует в списке на размещение') 
                             hashMap.put("ShowScreen", "wms.Ввод товара размещение")
                         else:
                             hashMap.put("ShowScreen", "wms.Ввод количества размещение")
@@ -1148,7 +1176,10 @@ def on_units_input(hashMap,_files=None,_data=None):
                             hashMap.put("art", '')
                             hashMap.put("nom_id", '')
                             hashMap.put("unit", '')
-                            hashMap.put("toast", 'Указанный товар отсутствует в заказе на отбор') 
+                            if user_locale == 'ua':
+                                hashMap.put("toast", 'Вказаний товар відсутній у заказі на відбір') 
+                            elif user_locale == 'ru':
+                                hashMap.put("toast", 'Указанный товар отсутствует в заказе на отбор') 
                             hashMap.put("ShowScreen", "wms.Ввод товара отбор")
 
                         else:
@@ -1160,7 +1191,10 @@ def on_units_input(hashMap,_files=None,_data=None):
                         hashMap.put("ShowScreen", "wms.Ввод количества инвентаризация")
 
                 else:    
-                    hashMap.put("toast", f"Товар с штрихкодом {barcode} не найден")        
+                    if user_locale == 'ua':
+                        hashMap.put("toast", f"Товар з штрихкодом {barcode} не знайдено")        
+                    elif user_locale == 'ru':
+                        hashMap.put("toast", f"Товар с штрихкодом {barcode} не найден")        
             else:
                 hashMap.put("toast", f'Error: {response.status_code}')
             
