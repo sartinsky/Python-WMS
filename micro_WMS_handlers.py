fb03790b77779a4e0675d56e051456c7d5ef0d62
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-07-30 18:14:12 +0300

    prod8

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -806,6 +806,7 @@ def on_input_qtyfact(hashMap,_files=None,_data=None):
             
             #Параметры запроса (например, фильтрация данных)
             data = {
+            "order_id": str(order_id),
             "no_order": str(no_order),
             "qty": hashMap.get("qty"),
             "sku_id": hashMap.get("nom_id"),
@@ -815,8 +816,8 @@ def on_input_qtyfact(hashMap,_files=None,_data=None):
             }
 
             try:
-                # Отправка POST-запроса
-                response = requests.post(url, json=data, timeout=timeout)
+                # Отправка patch-запроса
+                response = requests.patch(url, json=data, timeout=timeout)
 
                 # Проверка статуса ответа
                 if response.status_code == 201:
