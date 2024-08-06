8371b7df4a0a808c49c77473e920a86bd6defc96
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-08-06 12:19:47 +0300

    prod72

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -526,10 +526,10 @@ def on_btn_done(hashMap,_files=None,_data=None):
 
     CurScreen = hashMap.get("current_screen_name")
     user_locale = hashMap.get("USER_LOCALE")
-    orderIsManual = hashMap.get("Doc_Updated") 
+    orderIsManual = hashMap.get("orderIsManual") 
     Doc_Updated = hashMap.get("Doc_Updated")
 
-    if orderIsManual: 
+    if orderIsManual == 'true': 
         if not Doc_Updated is None or Doc_Updated == False:
             if user_locale == 'ua':
                 hashMap.put("toast", 'Документ не оновлено у БУ базі. Спробуйте ще раз')
