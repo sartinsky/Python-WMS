afafc1c0e5b2dfd55432c5e68d92ce0539c17950
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-08-12 16:50:16 +0300

    TEST10

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -979,7 +979,8 @@ def on_input_qtyfact(hashMap,_files=None,_data=None):
     elif CurScreen == "wms.Ввод количества взять":
 
         if listener is None:
-        
+
+            hashMap.put("toast", '1')    
             hashMap.put("qty_minus", str(-1*int(hashMap.get("qty"))))
 
             # Путь к нужной таблице или представлению
