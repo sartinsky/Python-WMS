54e6e62330bfded4a69b950b23ce46a94908a928
Oleksii Sartynskyi <sartinsky@gmail.com>
2024-08-12 14:57:12 +0300

    TEST3

--- micro_WMS_handlers.py
+++ micro_WMS_handlers.py
@@ -528,8 +528,8 @@ def on_FORVARD_BUTTON(hashMap, _files=None, _data=None):
         hashMap.put("ShowScreen", "wms.Ввод товара по заказу")
     elif CurScreen == "wms.Ввод количества факт" and listener is None:
         on_input_qtyfact(hashMap)
-        hashMap.put('qty_plan', None)
-        hashMap.put('qty', None)
+        hashMap.put('qty_plan', '')
+        hashMap.put('qty', '')
     return hashMap 
 
 def on_btn_put(hashMap, _files=None, _data=None):
